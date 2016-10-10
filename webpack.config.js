9066da69f9a17b168989ecaa481856e3a3f82628
menuka <=>
2016-10-10 21:51:28 +0530

    Webpack with demo file bundle

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const webpack = require('webpack');
+
+module.exports = {
+	entry: './src/app.js',
+	output: {
+		path: './bin',
+		filename: 'app.bundle.js'
+	},
+	module: {
+		loaders: [{
+			test: /\.js$/,
+			exclude: /node_modules/,
+			loader: 'babel-loader'
+		}]
+	},
+	plugin: [
+		new webpack.optimize.UglifyJsPlugin({
+			compress: {
+				warnings: false,
+			},
+			output: {
+				comments: false
+			}
+		}),
+	]
+
+}
